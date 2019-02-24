f2d4c900bdbbbb08e15a964e5624f1d73086eb73
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-02-24 15:39:32 -0700

    Start app page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
+import { protoPractice } from './protoPractice'
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      allQuestions: protoPractice,
+      studyGuideQuestions: [],
+      practiceActive: false
+    }
+  }
+
+  // componentDidMount() {
+  //   this.setState({allQuestions: protoPractice})
+  // }
+  startPractice() {
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>ProtoPractice</h1>
-        </header>
+        <h1>ProtoPractice</h1>
+        <h2>Build your knowledge of array, object, and string prototypes!</h2>
+        <h3>For each question you'll see an image of a prototype problem with the prototype name missing. Choose the correct prototype from the answers listed below.</h3>
+        <h3>Answers missed with be added to your study guide for futher practice.</h3>
+        <button onClick={this.startPractice}>Start Practicing</button>
+        <button>Reset Study Guide</button>
+        <Question allQuestions/>
       </div>
     );
   }
