b98172e6770d823524572a990d9b74cbc6d17cf7
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-02-25 13:33:47 -0700

    Add fetch and add deploy on github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { protoPractice } from './protoPractice';
 import Question from './Question'
 
 class App extends Component {
@@ -7,26 +6,63 @@ class App extends Component {
     super();
 
     this.state = {
-      allQuestions: protoPractice,
+      allQuestions: [],
       studyGuideQuestions: [],
-      practiceActive: false
+      practiceActive: false,
+      currentQuestionIndex: 0,
+      error: '',
     }
   }
 
-  startPractice() {
+  componentDidMount() {
+    fetch('http://memoize-datasets.herokuapp.com/api/v1/JHprotoPractice')
+    .then(response => response.json())
+    .then(result => {
+      this.setState({
+        allQuestions: result.JHprotoPractice,
+      })
+    })
+    .catch(error => {
+      this.setState({error: error.message})
+    })
+  }
 
+
+  startPractice = () => {
+    this.setState({practiceActive: true})
+  }
+
+  nextQuestion = () => {
+    //need if conditional to reset when index is more than number of questions
+    this.setState({currentQuestionIndex: this.state.currentQuestionIndex + 1})
+  }
+
+  updateStudyGuide = (questionid) => {
+    const updatedStudyGuide = [...this.state.studyGuideQuestions, questionid]
+    localStorage.setItem("studyGuideQuestions", JSON.stringify(updatedStudyGuide))
+    this.setState({studyGuideQuestions: updatedStudyGuide})
   }
 
   render() {
+    const {allQuestions, currentQuestionIndex} = this.state
     return (
       <div className="App">
+        {this.state.error && <p>{this.state.error}</p>}
         <h1>ProtoPractice</h1>
         <h2>Build your knowledge of array, object, and string prototypes!</h2>
         <h3>For each question you'll see an image of a prototype problem with the prototype name missing. Choose the correct prototype from the answers listed below.</h3>
         <h3>Answers missed with be added to your study guide for futher practice.</h3>
         <button onClick={this.startPractice}>Start Practicing</button>
+        <button>Show Study Guide</button>
         <button>Reset Study Guide</button>
-        <Question allQuestions={this.state.allQuestions}/>
+        {
+          this.state.practiceActive &&
+          <Question 
+            currentQuestion={allQuestions[currentQuestionIndex]}
+            nextQuestion={this.nextQuestion}
+            updateStudyGuide={this.updateStudyGuide}
+          />
+        }
       </div>
     );
   }
