7ccc49301e408e705ed6691919e91f7431287361
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-02-25 13:32:33 -0700

    Updates

---
+++ precache-manifest.fba0c026bb61b9343ba802e3447b48bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd9779b6dfb16d6107b2",
+    "url": "/proto-practice/static/css/main.4057879e.chunk.css"
+  },
+  {
+    "revision": "cd9779b6dfb16d6107b2",
+    "url": "/proto-practice/static/js/main.cd9779b6.chunk.js"
+  },
+  {
+    "revision": "a2f49a262e1717fd4a16",
+    "url": "/proto-practice/static/js/runtime~main.a2f49a26.js"
+  },
+  {
+    "revision": "3f5c363e30833400c899",
+    "url": "/proto-practice/static/js/2.3f5c363e.chunk.js"
+  },
+  {
+    "revision": "2a273ce7ae422ac1dd5175be87579f63",
+    "url": "/proto-practice/index.html"
+  }
+];
