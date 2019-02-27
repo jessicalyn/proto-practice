b5531d9a41a82f5d8a1eae6cdc0899c766225cb5
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-02-27 05:19:45 -0700

    Finish Local Storage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import Question from './Question'
+import Start from './Start';
+import Question from './Question';
+import End from './End'
 
 class App extends Component {
   constructor() {
@@ -7,61 +9,118 @@ class App extends Component {
 
     this.state = {
       allQuestions: [],
-      studyGuideQuestions: [],
-      practiceActive: false,
-      currentQuestionIndex: 0,
-      error: '',
+      activeQuestions: [],
+      studyGuideQuestions: JSON.parse(localStorage.getItem("studyGuideQuestions")) || [],
+      practiceRound: "start",
+      //practiceRound options: "start" "flashcards" "end"
+      currentIndex: JSON.parse(localStorage.getItem("currentIndex")) || 0
     }
   }
 
   componentDidMount() {
+    console.log("componentDidMount")
     fetch('http://memoize-datasets.herokuapp.com/api/v1/JHprotoPractice')
     .then(response => response.json())
     .then(result => {
-      this.setState({
-        allQuestions: result.JHprotoPractice,
-      })
+      this.setState({allQuestions: this.randomize(result.JHprotoPractice)})
+      if(localStorage.hasOwnProperty("activeQuestions")) {
+        const storageQuestions = JSON.parse(localStorage.getItem("activeQuestions"));
+        this.setState({activeQuestions: storageQuestions})
+      }
     })
-    .catch(error => {
-      this.setState({error: error.message})
-    })
+    .catch(error => {throw new Error(error)})
   }
 
+  randomize = (array) => {
+    array = array.sort(() => 0.5 - Math.random());
+    return array;
+  }
+
+  saveToStorage = (name, dataset) => {
+    localStorage.setItem(name, JSON.stringify(dataset))
+  }
 
   startPractice = () => {
-    this.setState({practiceActive: true})
+    console.log("startPractice")
+    if(this.state.activeQuestions.length === 0) {
+      this.setState({activeQuestions: this.state.allQuestions})
+    }
+    this.setState({practiceRound: "flashcards"})
   }
 
   nextQuestion = () => {
-    //need if conditional to reset when index is more than number of questions
-    this.setState({currentQuestionIndex: this.state.currentQuestionIndex + 1})
+    const {activeQuestions, currentIndex} = this.state
+    let roundIndex = activeQuestions.length - 1
+    if(currentIndex === roundIndex) {
+      this.setState({practiceRound: "end"})
+    } else {
+      this.setState({currentIndex: currentIndex + 1}, 
+      this.saveToStorage("currentIndex", currentIndex + 1))
+    }
   }
 
-  updateStudyGuide = (questionid) => {
-    const updatedStudyGuide = [...this.state.studyGuideQuestions, questionid]
-    localStorage.setItem("studyGuideQuestions", JSON.stringify(updatedStudyGuide))
-    this.setState({studyGuideQuestions: updatedStudyGuide})
+  addToStudyGuide = (question) => {
+    const updateStudyGuide = [...this.state.studyGuideQuestions, question]
+    this.saveToStorage("studyGuideQuestions", updateStudyGuide)
+    this.setState({studyGuideQuestions: updateStudyGuide})
+  }
+
+  useStudyGuide = () => {
+    console.log("study guide", this.state.studyGuideQuestions)
+    this.saveToStorage("activeQuestions", this.state.studyGuideQuestions)
+    this.setState({
+      activeQuestions: this.state.studyGuideQuestions, 
+      studyGuideQuestions: [], 
+      currentIndex: 0,
+      practiceRound: "flashcards"
+    })
+  }
+
+  clearStorage = () => {
+    localStorage.clear()
+    this.resetGame()
+  }
+
+  resetGame = () => {
+    this.setState({
+      studyGuideQuestions: [], 
+      practiceRound: "start", 
+      currentIndex: 0,
+      activeQuestions: []
+    })
   }
 
   render() {
-    const {allQuestions, currentQuestionIndex} = this.state
+    console.log("app render")
+    const {activeQuestions, practiceRound, currentIndex, studyGuideQuestions} = this.state
+    // let currentScore = 30 - studyGuideQuestions.length
     return (
       <div className="App">
-        {this.state.error && <p>{this.state.error}</p>}
         <h1>ProtoPractice</h1>
         <h2>Build your knowledge of array, object, and string prototypes!</h2>
-        <h3>For each question you'll see an image of a prototype problem with the prototype name missing. Choose the correct prototype from the answers listed below.</h3>
-        <h3>Answers missed with be added to your study guide for futher practice.</h3>
-        <button onClick={this.startPractice}>Start Practicing</button>
-        <button>Show Study Guide</button>
-        <button>Reset Study Guide</button>
         {
-          this.state.practiceActive &&
-          <Question 
-            currentQuestion={allQuestions[currentQuestionIndex]}
-            nextQuestion={this.nextQuestion}
-            updateStudyGuide={this.updateStudyGuide}
-          />
+          practiceRound === "start" &&
+            <Start
+              startPractice={this.startPractice}
+              studyGuideQuestions={studyGuideQuestions}
+            />
+        }
+        {
+          practiceRound === "flashcards" &&
+            <Question 
+              currentQuestion={activeQuestions[currentIndex]}
+              nextQuestion={this.nextQuestion}
+              addToStudyGuide={this.addToStudyGuide}
+            />
+        }
+        {
+          practiceRound === "end" &&
+            <End
+            // currentScore={currentScore}
+            studyGuideQuestions={studyGuideQuestions}
+            clearStorage={this.clearStorage}
+            useStudyGuide={this.useStudyGuide}
+            />
         }
       </div>
     );
---
+++ src/Start.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+export default class Start extends Component {
+
+  render = () => {
+    return (
+      <div>
+        <h2>How to Play</h2>
+        <h3>For each question you will see an image of a prototype problem with the prototype name missing. Choose the correct prototype from the answers listed below.</h3>
+        <h3>Answers missed with be added to your study guide for futher practice.</h3>
+        <button className="large-button" onClick={this.props.startPractice}>Start Practicing</button>
+      </div>
+    )
+  }
+}
+
+//add optional show of you have questions waiting in study guide
