f2b861b022444e6ae122a641f3b53a771158b666
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-02-27 09:01:11 -0700

    CSS Updates

--- src/Start.js
+++ src/Start.js
@@ -4,9 +4,10 @@ export default class Start extends Component {
 
   render = () => {
     return (
-      <div>
+      <div className="question-container" >
         <h2>How to Play</h2>
-        <h3>For each question you will see an image of a prototype problem with the prototype name missing. Choose the correct prototype from the answers listed below.</h3>
+        <h3>For each question you will see an image of a prototype problem with the prototype name missing.</h3>
+        <h3>Choose the correct prototype from the answers listed below.</h3>
         <h3>Answers missed with be added to your study guide for futher practice.</h3>
         <button className="start-practice large-button" onClick={this.props.startPractice}>Start Practicing</button>
       </div>
